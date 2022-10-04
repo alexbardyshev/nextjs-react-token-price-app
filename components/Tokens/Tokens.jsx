3eb98f5dc0b360cc0260325ea29b3c9b2d62b4ac
Oleksìi <bardyshev@gmail.com>
2022-10-04 21:13:03 +0300

    Added styling for Token component

--- components/Tokens/Tokens.jsx
+++ components/Tokens/Tokens.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import styles from './Tokens.module.css';
 
-export function Tokens( {name} ) {
+export function Tokens( { name, price, symbol, marketcap, volume, image, priceChange, id } ) {
     return (
-        <div>
-            <h1>{name}</h1>
+        <div className={styles.token__container}>
+            <div className={styles.token__row}>
+                <div className={styles.token}>
+                    <img src={image} className={styles.token__img} alt={name} />
+                    <h1 className={styles.token__h1}>{name}</h1>
+                    <p className={styles.token__symbol}>{symbol}</p>
+                </div>
+                <div className={styles.token__data}>
+                    <p className={styles.token__price}>${price}</p>
+                    <p className={styles.token__volume}>${volume.toLocaleString()}</p>
+                    {priceChange < 0 ? (
+                        <p className={(styles.token__percent, styles.red)}>{priceChange.toFixed(2)}%</p>
+                    ) : (
+                        <p className={(styles.token__percent, styles.green)}>{priceChange.toFixed(2)}%</p>
+                    )}
+                    <p className={styles.token__marketcap}>
+                        Mkt Cap: ${marketcap.toLocaleString()}
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
